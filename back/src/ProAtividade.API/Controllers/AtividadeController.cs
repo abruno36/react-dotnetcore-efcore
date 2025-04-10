71293395a1f3fc75413c7a287138c0a6124dcd9c
Antonio Bruno <abruno36@gmail.com>
2025-04-10 08:45:25 -0300

    Atualizações - gerais

--- back/src/ProAtividade.API/Controllers/AtividadeController.cs
+++ back/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using ProAtividade.Api.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ProAtividade.API.Controllers
 {
@@ -6,10 +9,43 @@ namespace ProAtividade.API.Controllers
     [Route("api/[controller]")]
     public class AtividadeController : ControllerBase
     {
+        public List<Atividade> Atividades = new List<Atividade>() {
+            new Atividade { Id = 1, Titulo = "Estudar", Descricao = "Estudar C#", Prioridade = Prioridade.Alta },
+            new Atividade { Id = 2, Titulo = "Treinar", Descricao = "Exercício físico", Prioridade = Prioridade.Normal },
+            new Atividade { Id = 3, Titulo = "Descansar", Descricao = "Assistir algo", Prioridade = Prioridade.Baixa }
+        };      
+
         [HttpGet]
-        public string get()
+        public IEnumerable<Atividade> Get()
         {
-            return "método get";
+            return Atividades;
+            
+        }
+
+        [HttpGet("{id}")]
+        public Atividade  Get(int id)
+        {
+            return Atividades.FirstOrDefault(ativ => ativ.Id == id);
+        }
+
+        [HttpPost]
+        public IEnumerable<Atividade> Post([FromBody] IEnumerable<Atividade> atividades)
+        {
+            Atividades.AddRange(atividades);
+            return Atividades;
+        }   
+
+        [HttpPut("{id}")]
+        public Atividade Put(int id, Atividade atividade)
+        {
+            atividade.Id = atividade.Id+1;
+            return atividade;
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"método Delete id = {id}";
         }
     }
 }
--- back/src/ProAtividade.API/Program.cs
+++ back/src/ProAtividade.API/Program.cs
@@ -1,31 +1,50 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//builder.Services.AddDbContext<DataContext>(
+//   options => options.UseSqlite(builder.Configuration.GetConnectionString("Default"))
+//);
+
+//builder.Services.AddScoped<IAtividadeRepo, AtividadeRepo>();
+//builder.Services.AddScoped<IGeralRepo, GeralRepo>();
+//builder.Services.AddScoped<IAtividadeService, AtividadeService>();
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                // .AddJsonOptions(options =>
+                //         {
+                //             options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                //         }
+                //     );
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProAtividade.API", Version = "v1" });
+});
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI(c =>
+if (app.Environment.IsDevelopment())
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProAtividade.API v1");
-    c.RoutePrefix = "swagger";
-});
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProAtividade.API v1"));
+}
 
-
-//app.UseHttpsRedirection();
-
-app.UseRouting();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(option => option.AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowAnyOrigin());
 
 app.MapControllers();
 
