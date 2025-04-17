c1e0ae08e0222cbb4a97c0539e586b040f93c693
Antonio Bruno <abruno36@gmail.com>
2025-04-17 15:50:25 -0300

    Ajuste app.js rotas

--- front/pro-atividade-app/src/App.js
+++ front/pro-atividade-app/src/App.js
@@ -9,10 +9,11 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
-      <Route path="/atividades" element={<Atividade />} />
-      <Route path="/clientes" element={<Cliente />} />
+      <Route path="/atividades/*" element={<Atividade />} />
+      <Route path="/clientes/*" element={<Cliente />} />
       <Route path="/cliente/:id/atividade" element={<Atividade />} />
-      <Route path="/cliente/detalhe/:id?" element={<ClienteForm />} />
+      <Route path='/cliente/detalhe/' element={<ClienteForm />} />
+      <Route path='/cliente/detalhe/:id' element={<ClienteForm />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
